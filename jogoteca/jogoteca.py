09bc49cdd5dd17a692c4d89be1d8700e41f5bc11
Otavio Henrique de Lima e Silva <otaviohlsilva@gmail.com>
2024-06-25 21:48:31 -0300

    commit 5

--- jogoteca/jogoteca.py
+++ jogoteca/jogoteca.py
@@ -13,6 +13,26 @@ jogo2 = Jogo("God of War", "Hack n Slash", "PS2")
 jogo3 = Jogo("Mortal Kombat", "Luta", "PS2")
 lista = [jogo1, jogo2, jogo3]
 
+
+class Usuario:
+    def __init__(self, nome, nickname, senha):
+        self.nome = nome
+        self.nickname = nickname
+        self.senha = senha
+
+
+usuario1 = Usuario("Bruno Divino", "BD", "alohomora")
+usuario2 = Usuario("Camila Ferreira", "Mila", "paozinho")
+usuario3 = Usuario("Guilherme Louro", "Cake", "python_eh_vida")
+
+
+usuarios = {
+    usuario1.nickname: usuario1,
+    usuario2.nickname: usuario2,
+    usuario3.nickname: usuario3,
+}
+
+
 app = Flask(__name__)
 app.secret_key = "alura"
 
@@ -46,7 +66,7 @@ def criar():
 
 @app.route("/login")
 def login():
-    proxima = request.args.get('proxima')
+    proxima = request.args.get("proxima")
     return render_template("login.html", proxima=proxima)
 
 
@@ -57,20 +77,22 @@ def login():
     ],
 )
 def autenticar():
-    if "alohomora" == request.form["senha"]:
-        session["usuario_logado"] = request.form["usuario"]
-        flash(session["usuario_logado"] + ' logado com sucesso!')
-        proxima_pagina = request.form['proxima']
-        return redirect(proxima_pagina)
+    if request.form["usuario"] in usuarios:
+        usuario = usuarios[request.form["usuario"]]
+        if request.form["senha"] == usuario.senha:
+            session["usuario_logado"] = usuario.nickname
+            flash(usuario.nickname + " logado com sucesso!")
+            proxima_pagina = request.form["proxima"]
+            return redirect(proxima_pagina)
     else:
-        flash('Usuário não logado.')
+        flash("Usuário não logado.")
         return redirect(url_for("login"))
 
 
 @app.route("/logout")
 def sair():
     session["usuario_logado"] = None
-    flash('Logout efetuado com sucesso!')
+    flash("Logout efetuado com sucesso!")
     return redirect(url_for("index"))
 
 
